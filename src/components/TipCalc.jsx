9d7238942ac89448e2021f7cb6a88f07feb77bdf
devferx <ferkki@hotmail.com>
2021-10-30 16:37:31 -0400

    :lipstick: [ADD] Desktop view

--- src/components/TipCalc.jsx
+++ src/components/TipCalc.jsx
@@ -5,34 +5,40 @@ import "../styles/components/TipCalc.css";
 export const TipCalc = () => {
   return (
     <section className="tipCalc">
-      <div className="form-control">
-        <p className="tipCalc__label">Bill</p>
-        <input
-          className="tipCalc__input tipCalc__input--bg tipCalc__input--bg-dollar"
-          type="text"
-          placeholder="0"
-        />
-      </div>
-
-      <div>
-        <p className="tipCalc__label">Select Tip %</p>
-        <div className="tipCalc__tip-grid">
-          <button>5%</button>
-          <button>10%</button>
-          <button className="active">15%</button>
-          <button>25%</button>
-          <button>50%</button>
-          <input className="tipCalc__input" type="text" placeholder="Custom" />
+      <div className="tipCalc__info">
+        <div className="form-control">
+          <p className="tipCalc__label">Bill</p>
+          <input
+            className="tipCalc__input tipCalc__input--bg tipCalc__input--bg-dollar"
+            type="text"
+            placeholder="0"
+          />
         </div>
-      </div>
 
-      <div className="form-control">
-        <p className="tipCalc__label">Number of People</p>
-        <input
-          className="tipCalc__input tipCalc__input tipCalc__input--bg tipCalc__input--bg-person"
-          type="text"
-          placeholder="0"
-        />
+        <div>
+          <p className="tipCalc__label">Select Tip %</p>
+          <div className="tipCalc__tip-grid">
+            <button>5%</button>
+            <button>10%</button>
+            <button className="active">15%</button>
+            <button>25%</button>
+            <button>50%</button>
+            <input
+              className="tipCalc__input"
+              type="text"
+              placeholder="Custom"
+            />
+          </div>
+        </div>
+
+        <div className="form-control">
+          <p className="tipCalc__label">Number of People</p>
+          <input
+            className="tipCalc__input tipCalc__input tipCalc__input--bg tipCalc__input--bg-person"
+            type="text"
+            placeholder="0"
+          />
+        </div>
       </div>
 
       <div className="tipCalc__result">
